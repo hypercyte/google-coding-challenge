e02f2676d5907a9261715c98f5675d5b9b9fcebd
Mujahid <hypercyte@gmail.com>
2021-07-01 10:12:00 +0100

    initial playlist implementation.

--- src/main/java/com/google/VideoPlaylist.java
+++ src/main/java/com/google/VideoPlaylist.java
@@ -1,6 +1,16 @@
 package com.google;
 
+import java.util.List;
+
 /** A class used to represent a Playlist */
 class VideoPlaylist {
+    private final String name;
+    private final String id;
+    private final List<String> tags;
 
+    public VideoPlaylist(String name, String id, List<String> tags) {
+        this.name = name;
+        this.id = id;
+        this.tags = tags;
+    }
 }
